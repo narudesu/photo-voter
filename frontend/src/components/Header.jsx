e60b664d1e182bbad90cc2e52d5eba0203770f08
narudesu (Matej Smid) <narudesu@yahoo.com>
2017-01-05 23:12:40 +0100

    Add horizontal ruler

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -18,6 +18,7 @@ export default class Header extends React.Component {
             <NavLink to="/competitions">Competitions</NavLink>
           </ul>
         </nav>
+        <hr/>
       </header>
     )
   }
