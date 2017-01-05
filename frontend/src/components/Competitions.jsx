65dba2aae5188ba6996ee5304a2dfa54b39a6663
narudesu (Matej Smid) <narudesu@yahoo.com>
2017-01-06 00:01:57 +0100

    Add color-coded state of Competitions List

--- frontend/src/components/Competitions.jsx
+++ frontend/src/components/Competitions.jsx
@@ -23,12 +23,24 @@ export default class Competitions extends React.Component {
       <ul>
         {this.state.competitions.map((comp) => (
           <li key={comp.id}>
-            <Link to={`competitions/${comp.id}`}>
+            <Link to={`competitions/${comp.id}`} className={this.linkClasses(comp)}>
               {comp.name} ({comp.state})
             </Link>
           </li>
         ))}
       </ul>
     )
+  }
+
+  linkClasses(comp) {
+    const { state } = comp
+    switch (state) {
+      case 'finished':
+        return 'text-danger'
+      case 'ongoing':
+        return 'text-success'
+      case 'upcoming':
+        return 'text-info'
+    }
   }
 }
